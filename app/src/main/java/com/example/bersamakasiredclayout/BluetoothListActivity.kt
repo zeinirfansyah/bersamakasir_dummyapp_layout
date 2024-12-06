b6809bc1c99e55fb9def0c8892acb4076988a6c3
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-12-06 16:30:10 +0700

    feat [bluetooth list activity]: added bluetooth list activity

--- app/src/main/java/com/example/bersamakasiredclayout/BluetoothListActivity.kt
+++ app/src/main/java/com/example/bersamakasiredclayout/BluetoothListActivity.kt
@@ -1,21 +1,17 @@
 package com.example.bersamakasiredclayout
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
-class TransactionsActivity : AppCompatActivity() {
+class BluetoothListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_transactions)
-
+        setContentView(R.layout.activity_bluetooth_list)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,13 +19,9 @@ class TransactionsActivity : AppCompatActivity() {
         }
 
         val btnBack: ImageView = findViewById(R.id.btn_back)
-        val btnBluetoothList: ImageView = findViewById(R.id.bt_bluetoothList)
+
         btnBack.setOnClickListener {
             finish()
-        }
-        btnBluetoothList.setOnClickListener {
-            intent = Intent(this, BluetoothListActivity::class.java)
-            startActivity(intent)
         }
     }
 }
