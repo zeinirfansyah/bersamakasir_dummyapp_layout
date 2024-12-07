93e7e8e4e11c77716be6772b3b076cfedaff6c0d
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-12-07 13:16:36 +0700

    feat [payment_submethod]: added payment submethod option

--- app/src/main/java/com/example/bersamakasiredclayout/TransactionsActivity.kt
+++ app/src/main/java/com/example/bersamakasiredclayout/TransactionsActivity.kt
@@ -9,10 +9,12 @@ import android.view.Gravity
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.card.MaterialCardView
 
 
 class TransactionsActivity : AppCompatActivity() {
@@ -30,6 +32,7 @@ class TransactionsActivity : AppCompatActivity() {
         val btnBack: ImageView = findViewById(R.id.btn_back)
         val btnBluetoothList: ImageView = findViewById(R.id.bt_bluetoothList)
         val btnBayar: Button = findViewById(R.id.btn_bayar)
+
 
         btnBack.setOnClickListener {
             finish()
@@ -57,7 +60,50 @@ class TransactionsActivity : AppCompatActivity() {
             }
 
             dialogPaymentMethod.show()
+
+            val ivEdc: ImageView = dialogBinding.findViewById(R.id.iv_edc)
+
+            ivEdc.setOnClickListener {
+                // Inflate the sub-dialog layout
+                val subDialogBinding = layoutInflater.inflate(R.layout.custom_dialog_payment_submethod, null)
+                val subDialogPaymentMethod = Dialog(this)
+                subDialogPaymentMethod.setContentView(subDialogBinding)
+                subDialogPaymentMethod.setCancelable(true)
+
+                val window2 = subDialogPaymentMethod.window
+                if (window2 != null) {
+                    val params = window2.attributes
+                    params.width = WindowManager.LayoutParams.MATCH_PARENT
+                    window2.attributes = params
+                }
+
+                subDialogPaymentMethod.show()
+
+                val tvDebit : MaterialCardView = subDialogBinding.findViewById(R.id.tv_debit)
+                val tvQris : MaterialCardView = subDialogBinding.findViewById(R.id.tv_qris)
+                val tvKartu : MaterialCardView = subDialogBinding.findViewById(R.id.tv_kartu)
+
+                tvDebit.setOnClickListener {
+                    subDialogPaymentMethod.dismiss()
+                    dialogPaymentMethod.dismiss()
+                    Toast.makeText(this, "Data sedang dikirim ke EDC...", Toast.LENGTH_SHORT).show()
+                }
+
+                tvQris.setOnClickListener {
+                    subDialogPaymentMethod.dismiss()
+                    dialogPaymentMethod.dismiss()
+                    Toast.makeText(this, "Data sedang dikirim ke EDC...", Toast.LENGTH_SHORT).show()
+                }
+
+                tvKartu.setOnClickListener {
+                    subDialogPaymentMethod.dismiss()
+                    dialogPaymentMethod.dismiss()
+                    Toast.makeText(this, "Data sedang dikirim ke EDC...", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
+
+
 
     }
 }
